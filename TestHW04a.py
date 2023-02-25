a2134d7d90cd3445080d9c7c542d32a4e255162e
SurajSanjaySingh <singhsurajs1311@gmail.com>
2023-02-24 20:50:38 -0500

    Add files via upload

---
+++ TestHW04a.py
@@ -0,0 +1,33 @@
+"""
+Created on Fri Feb 22 19:56:09 2023
+
+@author: Suraj
+
+Homework: 04a - Develop with the Perspective of the Tester in mind
+"""
+
+import unittest
+from unittest.mock import patch
+from HW04a import getRepo
+from HW04a import getCommit
+
+
+class TestApi(unittest.TestCase):
+    @patch('g1.requests.get')
+    
+
+#Testing Valid user name :
+    
+    def testValidUser(self,mock):
+        self.assertEqual(getRepo('https://api.github.com/users/richkempinski/repos'), 'Success')
+       
+#Testing number of commits within repository :
+    
+    def testCommits(self):
+        user = 'richkempinski'
+        name = 'threads-of-life'
+        self.assertEqual(getCommit(user,name),1)
+       
+
+if __name__ == '__main__':
+   unittest.main()
