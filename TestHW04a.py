f4f287fb5649d7eea8c2f82a58a48ab3122bdbec
SurajSanjaySingh <singhsurajs1311@gmail.com>
2023-03-21 23:35:52 -0400

    HW05a - mocking

--- TestHW04a.py
+++ TestHW04a.py
@@ -8,28 +8,31 @@ Homework: 04a - Develop with the Perspective of the Tester in mind
 
 import unittest
 from unittest.mock import patch
+import HW04a
 from HW04a import getRepo
 from HW04a import getCommit
 from HW04a import my_brand
-my_brand("HW04a-Develop-with-the-Perspective-of-the-Tester-in-mind")
+from unittest import mock
+
+my_brand("HW05a-Isolate External Dependencies")
 
 
 class TestApi(unittest.TestCase):
-    @patch('HW04a.requests.get')
-    
+       
+    @mock.patch('HW04a.getRepo')
+    def test_mock_1(self,test1):
+        test1.return_value="Success"
+        result = HW04a.getRepo("https://api.github.com/users/richkempinski/repos")
+        self.assertEqual(result,"Success")
 
-#Testing Valid user name :
-    
-    def testValidUser(self,mock):
-        self.assertEqual(getRepo('https://api.github.com/users/richkempinski/repos'), 'Success')
-       
-#Testing number of commits within repository :
-    
-    def testCommits(self):
-        user = 'richkempinski'
-        name = 'try_nbdev'
-        self.assertEqual(getCommit(user,name),2)
-       
+    @mock.patch('HW04a.getCommit')
+    def test_mock_2(self,test2):
+        test2.return_value=2
+        result = HW04a.getCommit('richkempinski','try_nbdev')
+        self.assertEqual(result,2)
+
 
 if __name__ == '__main__':
    unittest.main()
+   
+my_brand("HW05a-Isolate External Dependencies")
